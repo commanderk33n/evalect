91e0f9ce57c7405c9d8e3e0e8e8cd367df9787c6
eikood <hanfaplast@googlemail.com>
2014-12-09 10:55:37 +0100

    re- factoring/- formating code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update, :destroy]
-  before_action :correct_user,   only: [:edit, :update]
-  before_action :admin_user,     only: [:destroy, :index]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: [:destroy, :index]
+
   def index
     @users = User.paginate(page: params[:page])
   end
@@ -51,6 +52,7 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
   end
+
   # Before filters
 
   # Confirms a logged-in user.
@@ -61,11 +63,13 @@ class UsersController < ApplicationController
       redirect_to login_url
     end
   end
+
   # Confirms the correct user.
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
   end
+
   # Confirms an admin user.
   def admin_user
     redirect_to(root_url) unless current_user.admin?
